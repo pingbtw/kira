77fe7af2b504176913643e15b8976d4aff50c1e1
nxls-dev <exiled.developer@gmail.com>
2019-03-11 09:42:36 -0400

    Last commit ignored the actual code behind commands

--- src/main/java/commands/EightBall.java
+++ src/main/java/commands/EightBall.java
@@ -1,4 +1,26 @@
 package commands;
 
-public class EightBall {
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import java.util.Random;
+
+public class EightBall extends Command {
+    public EightBall() {
+        this.name = "8ball";
+        this.aliases = new String[]{"8b"};
+        this.help = "Ask and you shall receive";
+    }
+    @Override
+    protected void execute(CommandEvent commandEvent) {
+        commandEvent.reply(eightBall());
+    }
+
+    public static String eightBall() {
+        String[] outcomes = new String[]{
+                "Certainly", "Decidedly so", "Unlikely", "I have a good feeling about this", "No.", "Yes", "Maybe"
+        };
+        int output = new Random().nextInt((7) +1) +1;
+        return outcomes[output];
+    }
 }
